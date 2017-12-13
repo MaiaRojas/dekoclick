c2bc766c96df21c159ca1bf97727065162c1cd16
Lupo Montero <lupomontero@gmail.com>
2017-12-12 19:55:03 -0500

    fix(functions): wraps promises for better error handling and renames function for more clarity

--- functions/index.js
+++ functions/index.js
@@ -5,7 +5,7 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const updateCohortUsers = require('./lib/updateCohortUsers');
 const updateCoursesIndex = require('./lib/updateCoursesIndex');
-const updateCohortDelete = require('./lib/updateCohortDelete');
+const deleteCohortRelatedData = require('./lib/deleteCohortRelatedData');
 
 
 admin.initializeApp(functions.config().firebase);
@@ -13,4 +13,4 @@ admin.initializeApp(functions.config().firebase);
 
 exports.updateCohortUsers = updateCohortUsers(functions, admin);
 exports.updateCoursesIndex = updateCoursesIndex(functions, admin);
-exports.updateCohortDelete = updateCohortDelete(functions, admin);
+exports.deleteCohortRelatedData = deleteCohortRelatedData(functions, admin);
